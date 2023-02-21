f5c7da141e5033a411f1937d99b3432246735afb
YoussefAyachi95 <youssef.ayachi@web.de>
2023-02-21 13:32:47 +0100

    Edit firebase config :bug:

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,12 +2,12 @@ import firebase from "firebase";
 
 
 const firebaseConfig = {
-  apiKey: meta.env.VITE_API_KEY,
-  authDomain: meta.env.VITE_AUTH_DOMAIN,
-  projectId: meta.env.VITE_PROJECT_ID,
-  storageBucket: meta.env.VITE_STORAGE_BUCKET,
-  messagingSenderId: meta.env.VITE_MESSAGING_SENDER_ID,
-  appId: meta.env.VITE_APP_ID
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
